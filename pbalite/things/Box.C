7aa07fcee261a5c5c7b8760c706c8bc5dd57908a
JacobJustice <jacobjustice@gmail.com>
2023-02-10 00:53:26 -0500

    started added collision detection

--- pbalite/things/Box.C
+++ pbalite/things/Box.C
@@ -6,12 +6,34 @@
 #include <iostream>
 
 using namespace std;
+using namespace pba;
 
-void pba::Box::Display()
+void Box::Display()
 {
     for (Plane plane: planes)
 	{
 		plane.Display();
+    }
+}
+
+/*
+ * location gets updated such that position + location*velocity the point
+ * on the plane that was intersected
+ */
+
+bool detect_intersection(Vector start, Vector end, double& location, int& plane_ind)
+{
+    for (Plane pl: planes)
+    {
+        double loc = 2.0;
+        if (pl.detectIntersection(start, end, location)
+        {
+            if (loc < location)
+            {
+                location = loc;
+                plane_ind = i;
+            }
+        }
     }
 }
 
